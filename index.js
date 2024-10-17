841ecc7e44e9d874011267d1263ad8dc041957d2
Duarda007 <00001099187424sp@al.educacao.sp.gov.br>
2024-10-17 13:49:54 -0300

    mudancas na aparecencia

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const avanca = document.querySelectorAll("btn-proximo");
 
 avanca.forEach(button=>{  
-button.addEventListener("click",function())
+button.addEventListener("click",function(){
+const atual = document.querySelector(".ativo");
+ const proximoPaso ="passo-" + this.getAtribuur("data-proximo");
+ atual.classList.remove("ativo");
+ document.getElementById(proximoPaso).classList.add("ativo");
+  })
 })
+
+
